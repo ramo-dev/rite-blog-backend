246143e3dc522dc3fafde9acdb0615c1a5ac72ed
ramo-dev <annuar.dev@gmail.com>
2024-01-27 10:41:46 +0300

    API endpoint to get a specific blog by ID

--- api.py
+++ api.py
@@ -8,9 +8,18 @@ CORS(api_bp)
 # Function to load blogs from a JSON file in the 'data' directory
 def load_blogs_from_file():
     file_path = os.path.join('data', 'db.json')
-    with open(file_path, 'r') as file:
-        blogs = json.load(file)
-    return blogs
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            if 'blogs' in data and isinstance(data['blogs'], list):
+                return data
+            else:
+                raise ValueError("INVALID DATA STRUCTURE: MISSING OR INVALID 'BLOGS' KEY")
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File '{file_path}' not found.")
+    except json.JSONDecodeError:
+        raise ValueError(f"Invalid JSON data in file '{file_path}'.")
+
 
 # API endpoint to get all blogs
 @api_bp.route('/blogs', methods=['GET'])
@@ -19,11 +28,13 @@ def get_blogs():
     return jsonify(blogs)
 
 # API endpoint to get a specific blog by ID
+# API endpoint to get a specific blog by ID
 @api_bp.route('/blogs/<int:blog_id>', methods=['GET'])
 def get_blog(blog_id):
     blogs = load_blogs_from_file()
-    blog = next((blog for blog in blogs if blog['id'] == blog_id), None)
+    blog = next((blog for blog in blogs['blogs'] if blog['id'] == blog_id), None)
     if blog:
         return jsonify(blog)
     else:
         return jsonify({"error": "Blog not found"}), 404
+
